c18fc3e35fa6bbba895e00ba6156d28acdf43e88
Poornima C R <poornimaraghunath18@gmail.com>
2024-01-17 15:37:19 +0530

    update

---
+++ terraform_workspaces/modules/main.tf
@@ -0,0 +1,17 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "ami" {
+  description = "This is the ami for the instance"
+}
+
+variable "instance_type" {
+  description = "This is the instance type"
+}
+
+
+resource "aws_instance" "example" {
+  ami = var.ami
+  instance_type = var.instance_type
+}
