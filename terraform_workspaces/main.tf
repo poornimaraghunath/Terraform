c72fce655ad9540e5acc7857625f184bf639ce1f
Poornima C R <poornimaraghunath18@gmail.com>
2024-01-17 17:18:23 +0530

    update

--- terraform_workspaces/main.tf
+++ terraform_workspaces/main.tf
@@ -8,6 +8,12 @@ variable "ami" {
 
 variable "instance_type" {
   description = "value"
+
+  default = {
+    "dev" = "t2.micro"
+    "stage" = "t2.medium"
+    "prod" = "t2.xlarge"
+  }
 }
 
 module "ec2_instance" {
